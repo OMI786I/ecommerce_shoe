16741fe6245a12859aa8fc364cc709e78276e90a
OMI786I <sohrawardy1998@gmail.com>
2024-09-24 22:37:35 +0600

    first commit

---
+++ src/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      This is home M<button className="btn"></button>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/shared/Navbar.jsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+  return <div>Here is Navbar</div>;
+};
+
+export default Navbar;
