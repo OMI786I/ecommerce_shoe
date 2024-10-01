21570706430cfe40f22c5fe3bd20a5b2cfbb93cd
OMI786I <sohrawardy1998@gmail.com>
2024-10-02 02:23:59 +0600

    details page for bags

--- src/customHook/useDetailsFetch.jsx
+++ src/customHook/useDetailsFetch.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
-const useDetailsFetch = (id, endPoint) => {
+import { useEffect } from "react";
+const useDetailsFetch = (id) => {
+  const [endPoint, setEndPoint] = useState("shoes");
+
   const { isPending, error, data, refetch } = useQuery({
     queryKey: ["repoData", id],
     queryFn: () =>
-      fetch(`http://localhost:5000/shoes/${id}`).then((res) => res.json()),
+      fetch(`http://localhost:5000/${endPoint}/${id}`).then((res) =>
+        res.json()
+      ),
   });
+  useEffect(() => {
+    if (!data) {
+      setEndPoint("bags");
+    }
+  }, [data]);
 
   return { isPending, error, data, refetch };
 };
