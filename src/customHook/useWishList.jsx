d696313dee30ef0d157b97635a9e06702b601f46
OMI786I <sohrawardy1998@gmail.com>
2024-10-06 15:55:06 +0600

    added not to save same data inwishlist functationality

--- src/customHook/useWishList.jsx
+++ src/customHook/useWishList.jsx
@@ -11,17 +11,39 @@ const useWishList = () => {
     const id = res._id;
     const finalData = { ...res, email, id };
     delete finalData._id;
+    // axios
+    //   .post("http://localhost:5000/wishlist", finalData)
+    //   .then((response) => {
+    //     if (response.data.insertedId) {
+    //       toast.success("Successfully added to wishlist");
+    //     }
+    //     console.log(response);
+    //   })
+    //   .catch((error) => {
+    //     toast.error("There was an error adding the data");
+    //     console.log(error);
+    //   });
+
     axios
-      .post("http://localhost:5000/wishlist", finalData)
-      .then((response) => {
-        if (response.data.insertedId) {
-          toast.success("Successfully added to wishlist");
-        }
-        console.log(response);
+      .post("http://localhost:5000/wishlist/check", {
+        email: finalData.email,
+        id: finalData.id,
       })
-      .catch((error) => {
-        toast.error("There was an error adding the data");
-        console.log(error);
+      .then((response) => {
+        if (response.data.exists) {
+          toast.error("Already in wishlist!");
+        } else {
+          axios
+            .post("http://localhost:5000/wishlist", finalData)
+            .then((response) => {
+              if (response.data.insertedId) {
+                toast.success("You have successfully added to wishlist");
+              }
+            })
+            .catch((error) => {
+              console.log("Error inserting new data:", error);
+            });
+        }
       });
   };
   return wishPost;
