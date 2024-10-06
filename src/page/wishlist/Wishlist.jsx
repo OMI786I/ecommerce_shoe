a1baf5b214492a08cd39d07f4178f64f94f85734
OMI786I <sohrawardy1998@gmail.com>
2024-10-06 23:40:59 +0600

    data fetch on wishlist page

--- src/page/wishlist/Wishlist.jsx
+++ src/page/wishlist/Wishlist.jsx
@@ -4,34 +4,50 @@ import useWishListFetch from "../../customHook/useWishListFetch";
 const Wishlist = () => {
   const { fetchData, refetch, isPending } = useWishListFetch();
   console.log(fetchData);
+
   if (isPending) {
     return (
-      <div className="flex justify-center ">
+      <div className="flex justify-center items-center min-h-screen">
         <span className="loading loading-spinner loading-lg"></span>
       </div>
     );
-  } else
-    return (
-      <div>
-        {fetchData
-          ? fetchData.map((res) => (
-              <div key={res._id} className="hero bg-base-200 min-h-screen">
-                <div className="hero-content flex-col lg:flex-row">
-                  <img
-                    src={res.image}
-                    className="max-w-sm rounded-lg shadow-2xl"
-                  />
-                  <div>
-                    <h1 className="text-5xl font-bold">{res.title}</h1>
-                    <p className="py-6">{res.description}</p>
-                    <button className="btn btn-primary">Add to cart</button>
-                  </div>
-                </div>
+  }
+
+  return (
+    <div className="">
+      {fetchData && fetchData.length > 0 ? (
+        <div className="grid grid-cols-1">
+          {fetchData.map((res) => (
+            <div
+              key={res._id} // Add a unique key for each item
+              href="#"
+              className=" w-full h-64 flex items-center bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+            >
+              <img
+                className="w-60 h-60 object-contain"
+                src={res.image}
+                alt={res.title}
+              />
+              <div className="flex flex-col justify-between p-4 leading-normal">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  {res.title}
+                </h5>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  {res.description}
+                </p>
               </div>
-            ))
-          : ""}
-      </div>
-    );
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="flex justify-center items-center min-h-screen">
+          <p className="text-lg text-gray-600">
+            No items in your wishlist yet.
+          </p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Wishlist;
